2ed1846e5e37bc3993096b20652dfdf754b87f47
Steph Elsley <161695703+Stephh-E@users.noreply.github.com>
2024-10-17 20:11:17 +1100

    Addition of files

--- src/server.js
+++ src/server.js
@@ -4,31 +4,56 @@ const express = require("express");
 // eg. routes, settings
 const app = express();
 
+// GET localhost:3000/ 
+// .get("/", (req, res) => {})
+// .get("/", (banana, elephant) => {})
+
+/*
+instance.verb(routePath,
+	middleware 1,
+	middleware 2,
+	middleware 3,
+	middleware 4,
+	route handler
+	)
+*/
+
 function xanderMiddleware (request, response, next){
-    request.customData.xander = "cool programmer!"
-    next();
+	console.log("Xander middleware is now running!");
+	request.coolCoderAcademyStuff = {
+		...request.coolCoderAcademyStuff,
+		xander: "cool programmer!"
+	};
+	// This doesn't work because request.coolCoderAcademyStuff wasn't defined yet 
+	// request.coolCoderAcademyStuff.xander = "cool programmer!"
+	next();
 }
 
-
 app.get("/", 
-    // middleware function goes here
-    xanderMiddleware,
-    (request, response) => {
-    // response.send("<h1>Hello world!</h1>");
-    response.json({
-        message:"Hello world!",
-        customStuff: request.coolCoderAcademyStuff
-    });
+	// middleware function goes here
+	xanderMiddleware,
+	(request, response) => {
+	// response.send("<h1>Hello world!</h1>");
+	response.json({
+		message:"Hello world!",
+		customStuff: request.coolCoderAcademyStuff
+	});
 });
 
-app.post("/", (request, response) => {
+app.post("/", xanderMiddleware, (request, response) => {
 	response.json({
 		message: "POST request received!"
 	});
 });
 
 // http://localhost:3000/bananas
-app.post("/bananas", (request, response) => {
+app.post(
+	"/bananas", 
+	(request, response, next) => {
+		console.log("Bananas route has run"); 
+		next();
+	}, 
+	(request, response) => {
 	response.json({
 		message: "POST bananas received!"
 	});
